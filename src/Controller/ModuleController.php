730cfdcfaa52b4a3ec7c26d5990dd578c0cbef8a
TAPLIN <taplincorentin@gmail.com>
2023-08-22 15:41:44 +0200

    addModuleForm

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Module;
+use App\Form\ModuleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ModuleController extends AbstractController
 {
@@ -14,5 +18,30 @@ class ModuleController extends AbstractController
         return $this->render('module/index.html.twig', [
             'controller_name' => 'ModuleController',
         ]);
+    }
+
+    #[Route('/module/new', name: 'new_module')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response {
+
+        $module = new Module();
+
+        $form = $this->createForm(ModuleType::class, $module);
+
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $module = $form->getData();
+            $entityManager->persist($module); //prepare
+            $entityManager->flush(); //execute
+
+            return $this->redirectToRoute('app_session'); //redirect to sessionList
+
+        }
+
+        return $this->render('module/new.html.twig', [
+            'formAddModule' => $form,
+        ]);
+
     }
 }
--- src/Form/ModuleType.php
+++ src/Form/ModuleType.php
@@ -6,13 +6,16 @@ use App\Entity\Module;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ModuleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+        ->add('nom', TextType::class)
+        ->add('valider', SubmitType::class)
         ;
     }
 

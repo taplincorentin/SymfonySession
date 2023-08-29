4571975a524139386f17cc22b1adb0861aecbb6b
Corentin <taplincorentin@gmail.com>
2023-08-29 09:34:45 +0200

    session Repo comments

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -22,100 +22,99 @@ class SessionRepository extends ServiceEntityRepository
         parent::__construct($registry, Session::class);
     }
 
-    /** Afficher les stagiaires non inscrits */
+    /** Get stagaires that are not part of a session */
     public function findNonInscrits($session_id)
     {
         $em = $this->getEntityManager();
         $sub = $em->createQueryBuilder();
 
         $qb = $sub;
-        // sélectionner tous les stagiaires d'une session dont l'id est passé en paramètre
+        
         $qb->select('s')
             ->from('App\Entity\Stagiaire', 's')
-            ->leftJoin('s.sessions', 'se')
-            ->where('se.id = :id');
+            ->leftJoin('s.sessions', 'se')                              //select all stagiaires that are part of the session
+            ->where('se.id = :id'); 
         
         $sub = $em->createQueryBuilder();
-        // sélectionner tous les stagiaires qui ne SONT PAS (NOT IN) dans le résultat précédent
-        // on obtient donc les stagiaires non inscrits pour une session définie
+        
         $sub->select('st')
             ->from('App\Entity\Stagiaire', 'st')
-            ->where($sub->expr()->notIn('st.id', $qb->getDQL()))
-            // requête paramétrée
-            ->setParameter('id', $session_id)
-            // trier la liste des stagiaires sur le nom de famille
-            ->orderBy('st.nom');
+            ->where($sub->expr()->notIn('st.id', $qb->getDQL()))        //select all stagiaires that are NOT IN previous query
+            ->setParameter('id', $session_id)                           //we get all stagiaires that are not part of the session
+            ->orderBy('st.nom');                                        //sort by name
         
-        // renvoyer le résultat
-        $query = $sub->getQuery();
+        $query = $sub->getQuery();                                       //return result
         return $query->getResult();
     }
 
-    /*récupérer les modules non programmés dans une session donnée*/
-    public function findNonProgramme($session_id){
-        $em = $this->getEntityManager();
-        $sub = $em->createQueryBuilder();
 
-        $qb = $sub;
-        // sélectionner tous les modules d'une session dont l'id est passé en paramètre
-        $qb->select('p')
-            ->from('App\Entity\Programme', 'p')
-            ->leftJoin('p.session', 's')
-            ->where('s.id = :id');
-
-        $sub = $em->createQueryBuilder();
-        // sélectionner tous les modules qui ne SONT PAS (NOT IN) dans le résultat précédent
-        // on obtient donc les modules non  pour une session définie
-        $sub->select('po')
-            ->from('App\Entity\Programme', 'po')
-            ->where($sub->expr()->notIn('po.id', $qb->getDQL()))
-            // requête paramétrée
-            ->setParameter('id', $session_id);
-    
-        // renvoyer le résultat
-        $query = $sub->getQuery();
-
-        return $query->getResult();
-    }
-
-    public function findPastSessions(){             //method to get finished sessions
+    /** Method to get all finished sessions **/
+    public function findPastSessions(){
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
 
         $qb->select('s')
             ->from('App\Entity\Session', 's')
-            ->where('s.dateFin < CURRENT_TIMESTAMP()')
+            ->where('s.dateFin < CURRENT_TIMESTAMP()')                     //select all sessions with an endDate that has already past
             ->orderBy('s.dateDebut');
         
         $query = $qb->getQuery();
         return $query->getResult();
     }
 
-    public function findFutureSessions(){           //method to get future sessions
+    /** Method to get all future sessions **/
+    public function findFutureSessions(){ 
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
 
    
         $qb->select('s')
             ->from('App\Entity\Session', 's')
-            ->where('s.dateDebut > CURRENT_TIMESTAMP()')
+            ->where('s.dateDebut > CURRENT_TIMESTAMP()')                    //select all sessions with a startDate that is in the future
             ->orderBy('s.dateDebut');
         
         $query = $qb->getQuery();
         return $query->getResult();
     }
 
-    public function findCurrentSessions(){
+
+    /** Method to get all current sessions **/
+    public function findCurrentSessions(){        
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
 
         $qb->select('s')
             ->from('App\Entity\Session', 's')
-            ->where('s.dateDebut < CURRENT_TIMESTAMP()')
-            ->andWhere('s.dateFin > CURRENT_TIMESTAMP()')
+            ->where('s.dateDebut < CURRENT_TIMESTAMP()')                    //select all sessions with a startDate that ihas already past
+            ->andWhere('s.dateFin > CURRENT_TIMESTAMP()')                   //and an endDate that is in the future
             ->orderBy('s.dateDebut');
         
         $query = $qb->getQuery();
+        return $query->getResult();
+    }
+
+    
+    /** Get modules that are not part of a session **/
+    public function findNonProgramme($session_id){
+        $em = $this->getEntityManager();
+        $sub = $em->createQueryBuilder();
+
+        $qb = $sub;
+        $qb->select('p')
+            ->from('App\Entity\Programme', 'p')
+            ->leftJoin('p.session', 's')
+            ->where('s.id = :id');
+
+        $sub = $em->createQueryBuilder();
+
+        $sub->select('po')
+            ->from('App\Entity\Programme', 'po')
+            ->where($sub->expr()->notIn('po.id', $qb->getDQL()))
+            ->setParameter('id', $session_id);
+    
+
+        $query = $sub->getQuery();
+
         return $query->getResult();
     }
 }
